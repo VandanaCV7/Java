f4fc460b616a55d9d0cf2821fdbbcaf7af0eaea2
VandanaCV7 <cvvandana77@gmail.com>
2025-05-28 11:34:44 +0530

    "New Commit from VS Code"

--- DS1 - Searching Algo/InterpolationSearchRecursive.java
+++ DS1 - Searching Algo/InterpolationSearchRecursive.java
@@ -4,7 +4,7 @@ public class InterpolationSearchRecursive {
             int arr[] = {2,4,6,8,10,12,14};
             int search = 8;
             int hi = arr.length;
-            int index
+            
 
     }
     public static int interpolation(int arr[], int lo, int hi, int x){
---
+++ Practice/P1Sum.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class P1Sum {
+    public static void main(String[] args) {
+        int sum = 0, n=10;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+    System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+    }
+}
