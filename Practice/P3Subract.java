0c7e92d1b9aed96a54a52c7a5d5caf026cdd3434
VandanaCV7 <cvvandana77@gmail.com>
2025-05-28 15:47:27 +0530

    "Actions Check Commit"

---
+++ Practice/P3Subract.java
@@ -0,0 +1,12 @@
+public class P3Subract {
+public static void main(String[] args) {
+    int a = 20;
+    int b = 10;
+
+    // Subtract b from a
+    int result = a - b; // Perform subtraction
+
+    System.out.println("The result of " + a + " - " + b + " is: " + result); // Output the result   
+}
+    
+}
