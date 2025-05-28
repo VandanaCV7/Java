d6163697da9803f6695fb39c5fcaa9d4d1d9b9b2
VandanaCV7 <cvvandana77@gmail.com>
2025-05-29 01:24:59 +0530

    Right Pyramid

---
+++ Pattern/RightPyramid.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class RightPyramid {
+    public static void main(String[] args) {
+        int n=7;
+        for(int i=0;i<n;i++){
+            for(int j=n;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
