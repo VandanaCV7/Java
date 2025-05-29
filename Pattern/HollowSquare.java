7228ab071d61b8342c4c3fc8e500716cbd522e89
VandanaCV7 <cvvandana77@gmail.com>
2025-05-29 11:15:08 +0530

    Hollow Square

--- Pattern/HollowSquare.java
+++ Pattern/HollowSquare.java
@@ -2,7 +2,7 @@ package Pattern;
 public class HollowSquare {
 
     public static void main(String[] args) {
-        int n=4;
+        int n=5;
         for(int i=0;i<=n;i++){
             for(int j=0;j<=n;j++){
                 if(i==0 || i==n || j==0 || j==n){
