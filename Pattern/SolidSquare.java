4ce257cba98bd257e0b541bc11953e17650db07f
VandanaCV7 <cvvandana77@gmail.com>
2025-05-29 11:10:15 +0530

    Solid Square

---
+++ Pattern/SolidSquare.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class SolidSquare {
+    public static void main(String[] args) {
+        int n=5;
+        for(int a=0;a<n;a++){
+            for(int h=0;h<n;h++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
